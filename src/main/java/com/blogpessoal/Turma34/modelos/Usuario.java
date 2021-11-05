a0db6ea5cbb879ab2a0cef26d45c8a821467fdbe
unknown <thsimandi@gmail.com>
2021-11-05 15:07:10 -0300

    [Feature Mira - Task: Implemented class Usuario]

--- src/main/java/com/blogpessoal/Turma34/modelos/Usuario.java
+++ src/main/java/com/blogpessoal/Turma34/modelos/Usuario.java
@@ -1,11 +1,59 @@
 package com.blogpessoal.Turma34.modelos;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+/**
+ * 
+ * Classe espelho da tabela usuario no banco db_blogpessoal
+ * 
+ */
+
+@Entity
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idUsuario;
+
 	private String nome;
+
 	private String email;
+
 	private String senha;
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
