fecdd17fdbd35ddd103020928ef4f544b9fa8dbe
unknown <thsimandi@gmail.com>
2021-11-09 06:59:04 -0300

    [Feature Mira - Task: Add method salvar]

--- src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
+++ src/main/java/com/blogpessoal/Turma34/controladores/UsuarioControlador.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,13 +33,18 @@ public class UsuarioControlador {
 	}
 
 	@GetMapping("/{id_usuario}")
-	public ResponseEntity<Usuario> pegarId(@PathVariable(value = "id_usuario") Long idUsuario){
+	public ResponseEntity<Usuario> pegarId(@PathVariable(value = "id_usuario") Long idUsuario) {
 		Optional<Usuario> objetoOptional = repositorio.findById(idUsuario);
-		
-		if (objetoOptional.isPresent()){
+
+		if (objetoOptional.isPresent()) {
 			return ResponseEntity.status(200).body(objetoOptional.get());
 		} else {
 			return ResponseEntity.status(204).build();
 		}
+	}
+
+	@PostMapping("/salvar")
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario novoUsuario) {
+		return ResponseEntity.status(201).body(repositorio.save(novoUsuario));
 	}
 }
