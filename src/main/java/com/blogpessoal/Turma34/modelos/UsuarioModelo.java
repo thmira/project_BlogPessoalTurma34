980363840bf92b03059200861ed50f5c5aba8513
unknown <thsimandi@gmail.com>
2021-11-11 12:18:56 -0300

    [Feature Mira - Task: Add minhasPostagens]

--- src/main/java/com/blogpessoal/Turma34/modelos/UsuarioModelo.java
+++ src/main/java/com/blogpessoal/Turma34/modelos/UsuarioModelo.java
@@ -1,12 +1,13 @@
 package com.blogpessoal.Turma34.modelos;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Classe espelho da tabela usuario no banco db_blogpessoal
@@ -21,36 +22,55 @@ public class UsuarioModelo {
     private @Email String email;
     private @NotBlank @Size(min = 5, max= 30) String senha;
 
+    @OneToMany(mappedBy = "criador", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties({"criador"})
+    private List<PostagemModelo> minhasPostagens = new ArrayList<>();
+
     public Long getIdUsuario() {
+
         return idUsuario;
     }
 
     public void setIdUsuario(Long idUsuario) {
+
         this.idUsuario = idUsuario;
     }
 
     public String getNome() {
+
         return nome;
     }
 
     public void setNome(String nome) {
+
         this.nome = nome;
     }
 
     public String getEmail() {
+
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
     }
 
     public String getSenha() {
+
         return senha;
     }
 
     public void setSenha(String senha) {
+
         this.senha = senha;
     }
 
+    public List<PostagemModelo> getMinhasPostagens() {
+        return minhasPostagens;
+    }
+
+    public void setMinhasPostagens(List<PostagemModelo> minhasPostagens) {
+        this.minhasPostagens = minhasPostagens;
+    }
 }
