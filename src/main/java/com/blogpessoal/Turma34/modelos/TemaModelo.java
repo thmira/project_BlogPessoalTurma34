d13ce275fb3e0d4e4ca13db9da1c1256cc9efed2
unknown <thsimandi@gmail.com>
2021-11-11 12:29:19 -0300

    [Feature Mira - Task: Add temaPostagem]

--- src/main/java/com/blogpessoal/Turma34/modelos/TemaModelo.java
+++ src/main/java/com/blogpessoal/Turma34/modelos/TemaModelo.java
@@ -1,10 +1,11 @@
 package com.blogpessoal.Turma34.modelos;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -16,20 +17,35 @@ public class TemaModelo {
     private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idTema;
     private @NotBlank String tema;
 
+    @OneToMany(mappedBy = "temaRelacionado", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties({"temaRelacionado"})
+    private List<PostagemModelo> postagens = new ArrayList<>();
+
     public Long getIdTema() {
+
         return idTema;
     }
 
     public void setIdTema(Long idTema) {
+
         this.idTema = idTema;
     }
 
     public String getTema() {
+
         return tema;
     }
 
     public void setTema(String tema) {
+
         this.tema = tema;
     }
 
+    public List<PostagemModelo> getPostagens() {
+        return postagens;
+    }
+
+    public void setPostagens(List<PostagemModelo> postagens) {
+        this.postagens = postagens;
+    }
 }
